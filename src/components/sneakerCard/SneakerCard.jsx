5955c36872466480764fd56bd34e3494d670e4e7
Alexandr Chernyshov <72153987+sairexx@users.noreply.github.com>
2022-05-27 00:48:30 +0700

    ADD BTN

--- src/components/sneakerCard/SneakerCard.jsx
+++ src/components/sneakerCard/SneakerCard.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import jordan from '../../assets/image5.jpg'
-import plusLogo from '../../assets/plusLogo.svg'
+import React, { useState } from 'react';
+import unfavorite from '../../assets/unfavorite.svg'
+import favorite from '../../assets/isFavorite.svg'
+import offPlusLogo from '../../assets/plusLogo.svg'
+import onPlusLogo from '../../assets/onPlus.svg'
+
 import './SneakerCard.scss'
 
 const SneakerCard = ({ id, name, price, img }) => {
+    const [isFavorite, setIsFavorite] = useState(false)
+    const [isPlus, setIsPlus] = useState(false)
+
+    const onClickFavorite = () => {
+        setIsFavorite(!isFavorite);
+    };
+
+    const onClickPlus = () => {
+        setIsPlus(!isPlus);``
+    };
+
     return (
         <div className='cardWrapper'>
+            <div className="favoriteLogo" onClick={() => onClickFavorite()}>
+                <img src={isFavorite ? favorite : unfavorite} alt="Не нравится" />
+            </div>
             <img src={img} className='itemPhoto' alt="ФотоТовара" />
             <h5>{name}</h5>
             <div className="priceBlock">
@@ -13,7 +30,7 @@ const SneakerCard = ({ id, name, price, img }) => {
                     <span>Цена:</span>
                     <b>{price} руб.</b>
                 </div>
-                <img className='addToCartBtn' src={plusLogo} alt="Добавить" />
+                <img onClick={() => onClickPlus()} className='addToCartBtn' src={isPlus ? onPlusLogo : offPlusLogo} alt="Добавить" />
             </div>
         </div>
     );
